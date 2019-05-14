21733814b63c7d07db5765edd4b6863429bd8d71
Madani Lainani <madani.lainani@gmail.com>
2019-05-14 19:22:00 +0200

    Handle IPv6 address argument properly (WIP)

--- cli.py
+++ cli.py
@@ -20,29 +20,56 @@ def new_build_tree(node, path):
     local_path = path.copy()
     local_path.append(node.keyword)
     cmd = ' '.join(local_path) + ' ?'
+    print(cmd)
     session.send(cmd)
     time.sleep(.2)
     output = session.recv(65535)
     lines = output.decode('utf8').split('\r\n')
 
+    # Handle the case where keyword and description are on different lines
+
+    standalone_keyword = None
+
     for line in lines[1:]:
-        if line.startswith('vyos') or line.startswith('Possible'):
-            continue
-        m = re.search(r'^[<|>|\w|\s|\(|\)|-]+', line)
+        # if line.startswith('vyos') or line.startswith('Possible'):
+        #     continue
+        # m = re.search(r'[<|>|\w|\s|\(|\)|\.|:|/|-]+', line)
+
+        m = re.search(r'^\s+[<|\w].*', line)
         if m:
             words = m.group(0).split()
 
             if len(words) > 1 or (len(words) == 1 and re.match(r'^[<|\w]', words[0])):
+
+                if standalone_keyword:
+                    description = ' '.join(words)
+                    # child = Node(keyword, description, [])
+                    child = Node(keyword, [])
+                    node.completions.append(child)
+                    standalone_keyword = None
+                    continue
+
                 keyword = words[0]
                 if len(keyword) > 13:
-                    print('Fix multiline completion for', len(keyword), '-char', keyword)
-                    sys.exit(1)
+                    standalone_keyword = keyword
+                    continue
+
                 # description = ' '.join(words[1:])
 
                 # Handle argument and command complete
-                m = re.match(r'<(\w+)>', keyword)
+                # m = re.match(r'<([\w|\.]+)>', keyword)
+                m = re.match(r'<(.+)>', keyword)
                 if m:
                     keyword = m.groups()[0].upper()
+                    # if keyword != 'ENTER':
+                    #     print(keyword)
+                    if keyword.startswith('X.X'):
+                        keyword = '1.1.1.1/8'
+                    if keyword.startswith('H:H'):
+                        keyword = 'AAAA::1/112'
+
+                if keyword == 'Invalid':
+                    sys.exit(1)
 
                 # child = Node(keyword, description, [])
                 child = Node(keyword, [])
@@ -66,7 +93,7 @@ if __name__ == "__main__":
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
-        client.connect('172.23.62.73', 22, 'vyos', 'vyos')
+        client.connect('10.211.55.7', 22, 'vyos', 'vyos')
     except paramiko.SSHException or paramiko.socket.error or socket.timeout:
         print("Can't connect!\n")
         sys.exit(1)
